019f813f96f3e56fe356888082fb247cd0d6706c
NajaNaja89 <najanaja89@gmail.com>
2018-10-01 16:25:41 +0600

    first commit

---
+++ CSharpPractModule01/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractModule01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вывести имя на консоль
+            Console.WriteLine("Ruslan Tyo");
+            Console.WriteLine();
+
+            Console.WriteLine("Enter number one ");
+            var digitTemp = Console.ReadLine();
+            int numberOne = int.Parse(digitTemp);
+
+            Console.WriteLine("Enter number two ");
+            digitTemp = Console.ReadLine();
+            int numberTwo = int.Parse(digitTemp);
+
+            Console.WriteLine("Enter number Three ");
+            digitTemp = Console.ReadLine();
+            int numberThree = int.Parse(digitTemp);
+
+
+            int sum = numberOne + numberTwo + numberThree;
+
+            Console.WriteLine();
+            Console.Write(sum);
+
+
+            Console.ReadLine();
+        }
+    }
+}
